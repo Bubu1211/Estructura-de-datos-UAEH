fc26ee23cf697c6e36a324079dacbf68cd972c0d
César Ricardo Lazcano Valdez <la384541@uaeh.edu.mx>
2021-01-20 18:04:00 -0600

    Programa Arreglos terminado

---
+++ Arreglos_act_1_4/arreglos.cpp
@@ -0,0 +1,22 @@
+///Escribir un programa que defina un vector de números y calcule la suma de sus elementos
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    int numeros[] = {1,2,3,4,5}; ///define el vector tipo int
+    int suma = 0; ///var acumuladora para la suma
+
+    ///ciclo que itera sobre el arreglo y suma sus elementos
+    for(int i = 0; i<5; i++)
+    {
+        suma += numeros[i];
+    }
+    cout<<"La suma de los elementos del vector es: "<<suma<<endl;
+
+    getch(); ///evita que se cierre la consola
+    return 0;
+}
