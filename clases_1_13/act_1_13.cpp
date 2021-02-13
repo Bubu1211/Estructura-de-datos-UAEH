33b4795d922c2681df16d29a9212f6fedd0643f2
--replace <la384541@uaeh.edu.mx>
2021-02-13 11:03:46 -0600

    Termina actividad 13, nota: mejora de los métodos y constructor

--- clases_1_13/act_1_13.cpp
+++ clases_1_13/act_1_13.cpp
@@ -1,9 +1,56 @@
 #include <iostream>
 
-using namespace std;
+class Cuenta_Bancaria
+{
+private:
+    int num_cuenta;
+    std::string nombre;
+    double saldo;
+public:
+    Cuenta_Bancaria();
+    void deposito(double);
+    void retiro(double);
+    void consulta_saldo();
+};
+
+Cuenta_Bancaria::Cuenta_Bancaria(){
+    std::cout<<"Ingrese el número de cuenta: ";
+    std::cin>>num_cuenta;
+    std::cout<<"Introduce el nombre del propietario: ";
+    std::cin>>nombre;
+    saldo = 0.0;
+}
+
+void Cuenta_Bancaria::deposito(double cantidad)
+{
+    saldo+=cantidad;
+    std::cout<<"\nSe realizó un deposito de: $"<< cantidad<<"\n";
+}
+
+void Cuenta_Bancaria::retiro(double cantidad)
+{
+    if(cantidad <= saldo){
+        saldo-=cantidad;
+        std::cout<<"\nSe realizo el retiro de: $"<<cantidad;
+    }else
+        std::cout<<"Saldo insuficiente\n";
+}
+
+void Cuenta_Bancaria::consulta_saldo()
+{
+    std::cout<<"\nSaldo: $"<<saldo;
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Cuenta_Bancaria *x = new Cuenta_Bancaria;
+
+    x->consulta_saldo();
+    x->deposito(1200);
+    x->retiro(900);
+    x->consulta_saldo();
+
+    delete x;
+
     return 0;
 }
