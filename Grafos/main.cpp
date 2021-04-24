8d677d2c4aa7f0133c36d6285098c085237e3453
Lazcano Valdez César Ricardo <la384541@uaeh.edu.mx>
2021-04-24 10:49:26 -0500

    Implementación de métodos sobre listas de adyacencia para representar grafos

--- Grafos/main.cpp
+++ Grafos/main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
+#include <bits/stdc++.h>
 
-using namespace std;
+///Grafos con lista de adyacencia
 
+///agregar una arista a un grafo no dirigidp
+void addEdge(std::vector<int> adj[], int u, int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+///Imprimir la lista de adyacencia
+void printGraph(std::vector<int> adj[], int V)
+{
+    for(int v = 0; v < V; v++)
+    {
+        std::cout<<v <<" = ";
+        for(auto x : adj[v])
+            std::cout<<"-> "<<x;
+
+        std::cout<<std::endl;
+    }
+}
 int main()
 {
-    cout << "Hello world!" << endl;
+    int V = 5;
+    std::vector<int> adj[V];
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 4);
+    addEdge(adj, 1, 2);
+    addEdge(adj, 1, 3);
+    addEdge(adj, 1, 4);
+    addEdge(adj, 2, 3);
+    addEdge(adj, 3, 4);
+    printGraph(adj, V);
+
     return 0;
 }
